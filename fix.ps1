1bfb1b1907103e725cca0fc3fcbf7dcef2d88814
IamDudditz <dudditzmk2@gmail.com>
2026-06-03 14:58:50 +0700

    Commit all

--- fix.ps1
+++
@@ -1,4 +0,0 @@
-$text = [IO.File]::ReadAllText('manager.bat')
-$text = $text.Replace("`r`n", "`n").Replace("`n", "`r`n")
-$utf8NoBom = New-Object System.Text.UTF8Encoding($False)
-[IO.File]::WriteAllText('manager.bat', $text, $utf8NoBom)
